7d2dafad24bfb765357b7db1e1a86da5cf5d3240
berzyvonne <berzyvonne@gmail.com>
2024-05-20 14:57:37 -0400

    create tables, connected to database

---
+++ server/index.js
@@ -0,0 +1,10 @@
+const { client } = require('./db');
+
+const init = async()=> {
+  await client.connect();
+  console.log('connected to database');
+  await createTables();
+  console.log('tables created');
+};
+
+init();
